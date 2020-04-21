57e66987b2e2de7764f6b58a12cd6257f19efa8a
Cesar Lara <cesar.lara@yale.edu>
2020-04-21 18:55:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ age = 18
 favorite_food = "pizza"
 height_in_inches = 70
 
-height_message = 
+height_message > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average" 
 
 ## Do not modify below this line!
 
